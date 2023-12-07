a9f57d73d34aa0133a4407ee397305a370c6ea6a
Khaled021103 <khaledhesham007@gmail.com>
2023-12-07 23:00:23 +0200

    Update Database.php

--- core/Database.php
+++ core/Database.php
@@ -8,10 +8,32 @@ class Database
 
     public function __construct(array $config) 
     {
-        $dsn = $config[''] ?? '';
-        $user = $config[''] ?? '';
-        $password = $config[''] ?? '';
+        $dsn = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $this->getAppliedMigrations();
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;");
+    }
+    public function getAppliedMigrations()
+    {
+        $statement = $this->pdo->prepare("SELECT migration FROM migrations");
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
     }
 }
